ddae771f22183bc264d69ee62ed6cb989cf2a213
lab3443 <lab3443@labsoft.local>
2023-05-25 10:58:23 -0300

    a

--- mysite/aaa/models.py
+++ mysite/aaa/models.py
@@ -1,13 +1,61 @@
 from django.db import models
 
-class Plane(models.Model):
-    plane = models.CharField(max_length = 100, unique = True)
+class Socio(models.Model):
+    Categorias = models.TextChoices("Categorias", " ALUNO PILOTO INSTRUTOR")
 
-class Maintenance(models.Model):
-    plane = models.ForeignKey("planes.Plane", on_delete = models.CASCADE)
-    date = models.DateField()
+    Matricula = models.CharField(max_lenght=100, unique=True)
+    Categoria = models.CharField(choices=Categorias, max_length=10)
+    Nome = models.CharField(max_lenght=100)
+    CPF = models.CharField(max_lenght=11)
+    Email = models.CharField(max_lenght=100)
+    DataDeNascimento = models.DateField()
+    Endereco = models.CharField(max_lenght=100)
     
     class Meta:
-        constraints =[
-            models.UniqueConstraint(fields=["date", "plane"], name = "uq_%(app_label)s_%(class)s_date")
-        ]
+        abstract = True
+        
+class Aluno(Socio):
+    Matricula = "ALUNO"
+    NotaPonderada = models.FloatField()
+    
+class Piloto(Socio):
+    Matricula = "PILOTO"
+    Breve = models.CharField(max_length=100)
+    
+class Instrutor(Piloto):
+    Matricula = "INSTRUTOR"
+    NomeDoCurso = models.CharField(max_length=100)
+    DataDiploma = models.DateField()
+    Instituicao = models.CharField(max_length=100)
+    
+class Funcionario(models.Model):
+    IdFuncionario = models.CharField(max_lenght=100, unique=True)
+    Nome = models.CharField(max_lenght=100)
+    CPF = models.CharField(max_lenght=11)
+    DataDeNascimento = models.DateField()
+    Endereco = models.CharField(max_lenght=100)
+    
+class Usuario(models.Model):
+    Socio = models.OneToOneField(
+        Socio,
+        on_delete=models.CASCADE,
+        primary_key=False
+    )
+    Funcionario = models.OneToOneField(
+        Funcionario,
+        on_delete=models.CASCADE,
+        primary_key=False
+    )
+    Username = models.CharField(max_lenght=50, unique=True)
+    Senha = models.CharField(max_lenght=50)
+    
+class Voo(models.Model):
+    TipoConceitos = models.TextChoices("TipoConceitos", " A B C D")
+    
+    IdVoo = models.CharField(max_length=100, unique=True)
+    Socio = models.ForeignKey(Socio, on_delete=models.CASCADE)
+    Instrutor = models.ForeignKey(Instrutor, on_delete=models.CASCADE)
+    Data = models.DateField()
+    HorarioSaida = models.TimeField()
+    HorarioChegada = models.TimeField()
+    Parecer = models.CharField(choices=TipoConceitos, max_length=1)
