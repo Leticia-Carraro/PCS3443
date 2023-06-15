2d59078bfec0d5b0979e241c76c9f4fbd96973ed
BrunoY <51705553+BrunoY-OG@users.noreply.github.com>
2023-06-15 09:56:40 -0300

    modified:   backend/models.py

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+import uuid
+
+class Usuario(models.Model):
+    id_usuario = models.UUIDField(primary_key = True, 
+                                    editable = False,
+                                    default = uuid.uuid4 )
+    username = models.CharField(max_length = 30, unique=True)
+    password = models.CharField(max_length = 300)
 
 class Funcionario(models.Model):
-    id_funcionario = models.Index()
+    id_funcionario = models.UUIDField(primary_key = True, 
+                                    editable = False,
+                                    default = uuid.uuid4 )
     nome = models.CharField(max_lenght = 100)
     cpf = models.CharField(max_length = 11)
     data_nascimento = models.DateField()
     endereco = models.CharField(max_length = 100)
-    
+    id_usuario = models.ForeignKey("usuario.Usuario", on_delete = models.DO_NOTHING)
 
 class Maintenance(models.Model):
     plane = models.ForeingKey("planes.Plane", on_delete = models.CASCADE)
