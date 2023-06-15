7a84fbdd0726b21f619272d293be27857c9a2ffc
BrunoY <51705553+BrunoY-OG@users.noreply.github.com>
2023-06-15 10:16:36 -0300

    modified:   backend/models.py

--- backend/models.py
+++ backend/models.py
@@ -2,26 +2,40 @@ from django.db import models
 import uuid
 
 class Usuario(models.Model):
-    id_usuario = models.UUIDField(primary_key = True, 
-                                    editable = False,
+    id_usuario = models.UUIDField(editable = False,
                                     default = uuid.uuid4 )
     username = models.CharField(max_length = 30, unique=True)
     password = models.CharField(max_length = 300)
+    id_funcionario = models.ForeignKey("funcionario.Funcionario", 
+                                       on_delete = models.DO_NOTHING,
+                                       null = True)
+    id_funcionario = models.ForeignKey("socio.Socio", 
+                                       on_delete = models.DO_NOTHING,
+                                       null = True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['id_usuario']),
+        ]
 
 class Funcionario(models.Model):
-    id_funcionario = models.UUIDField(primary_key = True, 
-                                    editable = False,
+    id_funcionario = models.UUIDField(editable = False,
                                     default = uuid.uuid4 )
     nome = models.CharField(max_lenght = 100)
     cpf = models.CharField(max_length = 11)
     data_nascimento = models.DateField()
     endereco = models.CharField(max_length = 100)
-    id_usuario = models.ForeignKey("usuario.Usuario", on_delete = models.DO_NOTHING)
+    class Meta:
+        indexes = [ models.Index(fields=['id_funcionario']) ],
+        ordering = ['nome']
 
-class Maintenance(models.Model):
-    plane = models.ForeingKey("planes.Plane", on_delete = models.CASCADE)
-    date = models.DateField()
-    
+class Voo(models.Model):
+    id_voo = models.UUIDField(editable = False,
+                                    default = uuid.uuid4 )
+    data = models.DateField()
+    horario_saida = models.TimeField()
+    horario_chegada = models.TimeField()
+    id_socio = models.ForeignKey("socio.Socio", on_delete= models.DO_NOTHING)
+    id_instrutor = models.ForeignKey
     class Meta:
         constraints =[
             models.UniqueConstraint(fields=["date", "plane"], name = "uq_%(app_label)s_%(class)s_date")
